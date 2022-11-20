5b80d75272c5e95bfab083dfe4330d76b2c552d2
cyschneck <22159116+cyschneck@users.noreply.github.com>
2022-11-19 21:45:45 -0700

    update version with star_data v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Python Package Setup
 from setuptools import setup, find_packages
 
-VERSION="0.1.1"
+VERSION="0.1.2"
 DESCRIPTION="A Python package to generate an astronomy star chart based on spherical projection that corrects for distortions to generate star charts with spherical projection"
 
 with open("README.md", "r") as f:
@@ -16,12 +16,12 @@ setup(
 	long_description=long_description_readme,
 	long_description_content_type='text/markdown',
 	url="https://github.com/cyschneck/Star-Chart-Spherical-Projection",
-	download_url="https://github.com/cyschneck/Star-Chart-Spherical-Projection/archive/refs/tags/v0.1.1.tar.gz",
+	download_url="https://github.com/cyschneck/Star-Chart-Spherical-Projection/archive/refs/tags/v{0}.tar.gz".format(VERSION),
 	author="cyschneck (C. Y. Schneck)",
 	keywords=["astronomy", "python", "star charts", "precession", "proper motion", "spherical projection"],
 	license="MIT",
 	classifiers=[
-		"Development Status :: 1 - Planning",
+		"Development Status :: 4 - Beta",
 		"Intended Audience :: Developers",
 		"Intended Audience :: Education",
 		"Intended Audience :: Science/Research",
@@ -30,7 +30,7 @@ setup(
 		"Programming Language :: Python :: 3.7",
 		"Topic :: Scientific/Engineering :: Astronomy"
 	],
-	packages=find_packages(include=['star_chart_spherical_projection', 'star_chart_spherical_projection.*']),
+	packages=find_packages(include=['star_chart_spherical_projection', 'star_chart_spherical_projection.*', 'star_data']),
 	install_requires=[
 		"configparser>=5.3.0",
 		"matplotlib>=3.1.0",

9012a4df2ac26d843d1689a1d4dacd47fbb212f6
cyschneck <22159116+cyschneck@users.noreply.github.com>
2025-05-11 00:56:19 -0600

    update comments

--- star_chart_spherical_projection/__init__.py
+++ star_chart_spherical_projection/__init__.py
@@ -1,26 +1,32 @@
-from .error_handling import errorHandling
-from .error_handling import errorHandlingStarClass
-from .error_handling import errorHandlingPredictPoleStar
-
+# generate_star_chart.py function calls
+from .generate_star_chart import plot_stereographic_projection
 from .generate_star_chart import _get_stars
 from .generate_star_chart import _ra_to_radians
 from .generate_star_chart import _radians_to_ra
 from .generate_star_chart import _generate_stereographic_projection
 from .generate_star_chart import _ra_dec_via_pm
 from .generate_star_chart import _precession_vondrak
-from .generate_star_chart import plot_stereographic_projection
 
+# position_of_stars.py function calls
 from .position_of_stars import final_position
 from .position_of_stars import position_over_time
 from .position_of_stars import plot_position
 from .position_of_stars import predict_pole_star
 
+# starClass.py function calls
 from .starClass import add_new_star
 
+# declination_r_axis.py function calls
 from .declination_r_axis import _calculate_ruler
 from .declination_r_axis import _calculate_radius_of_circle
 from .declination_r_axis import _calculate_length
 
+## Error Handling: error_handling.py function calls
+from .error_handling import errorHandling
+from .error_handling import errorHandlingStarClass
+from .error_handling import errorHandlingPredictPoleStar
+
+## TEMP: Vondrak in Python 3
 from .ra_dec_precession_vondrak import ltp_pbmat
 from .ra_dec_precession_vondrak import pdp
 from .ra_dec_precession_vondrak import ra_dec
